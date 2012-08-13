b64f90fbd747210cf1182e333b8456b7ba1aa659
Volker Bach <vbach@Volkers-MacBook-Pro-Retina.local>
2012-08-13 16:24:32 +0200

    Initial Commit

---
+++ URLEncoder/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  URLEncoder
+//
+//  Created by Volker Bach on 13.08.12.
+//  Copyright (c) 2012 Volker Bach. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
